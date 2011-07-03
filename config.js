3d8824d1e7a908e8533975d141592e23c3bf4756
Nightgunner5 (Ben L.) <nightgunner5@llamaslayers.net>
2011-07-03 15:10:41 -0500

    Initial commit

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+	port: 12345,
+	allowedAddresses: ['127.0.0.1', '127.0.0.2', '127.0.0.3']
+};
---
+++ pubcomp-central.js
@@ -0,0 +1,65 @@
+var net = require( 'net' ),
+	config = require( './config' );
+
+const VERSION = 1;
+
+var server = net.createServer();
+server.on( 'connection', handleConnection );
+server.listen( config.port );
+
+function handleConnection( socket ) {
+	if ( config.allowedAddresses.indexOf( socket.remoteAddress ) == -1 ) {
+		console.log( 'Invalid connection from ' + socket.remoteAddress );
+		socket.destroy();
+		return;
+	}
+	console.log( 'Connection from ' + socket.remoteAddress );
+
+	var buffer = '';
+
+	// Recieved/sent in ascii, because JSON escapes Unicode and ascii parsing is faster
+	socket.setEncoding( 'ascii' );
+
+	socket.setKeepAlive( true );
+
+	socket.on( 'data', function( data ) {
+		buffer += data;
+
+		if ( buffer.indexOf( '\0' ) ) {
+			var messages = buffer.split( '\0' );
+			buffer = messages.pop();
+			messages.forEach( function( message ) {
+				processMessage( JSON.parse( message ), socket );
+			} );
+		}
+	} );
+
+	socket.on( 'error', function( error ) {
+		console.log( error );
+	} );
+
+	socket.on( 'close', function( had_error ) {
+		console.log( 'Connection to ' + socket.remoteAddress + ' lost!' );
+	} );
+}
+
+function write( data, socket ) {
+	socket.write( JSON.stringify( data ) + '\0' );
+}
+
+function processMessage( data, socket ) {
+	switch ( data.type ) {
+		case 'hello':
+			if ( data.version != VERSION ) {
+				console.log( socket.remoteAddress + ' is version ' + data.version + ', expecting ' + VERSION );
+				write( { type: 'update', version: VERSION }, socket );
+			} else {
+				write( { type: 'hello', version: VERSION }, socket );
+			}
+			break;
+		default:
+			console.log( 'Got unknown message type ' + data.type );
+			console.log( data );
+			console.log( 'from ' + socket.remoteAddress );
+	}
+}
